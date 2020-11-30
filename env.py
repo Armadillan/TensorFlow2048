778cec6d96adbfd963f9eca4df22c78e87d3d3f4
Armadillan <antonikowalik23@gmail.com>
2020-11-30 14:59:44 -0500

    Update env.py

--- env.py
+++ env.py
@@ -6,6 +6,7 @@ import random
 import tensorflow as tf
 import numpy as np
 
+# unused imports are actually used in the console while developing
 from tf_agents.environments import py_environment
 from tf_agents.environments import tf_environment
 from tf_agents.environments import tf_py_environment
@@ -15,6 +16,8 @@ from tf_agents.environments import wrappers
 from tf_agents.trajectories import time_step as ts
 
 class PyEnv2048(py_environment.PyEnvironment):
+
+    MAX_MOVES = 1000
 
     def __init__(self):
 
@@ -32,6 +35,7 @@ class PyEnv2048(py_environment.PyEnvironment):
 
         self._episode_ended = False
 
+        self._moves = 0
     def action_spec(self):
         return self._action_spec
 
@@ -47,6 +51,8 @@ class PyEnv2048(py_environment.PyEnvironment):
         self._state[b[0]][b[1]] = 2
 
         self._episode_ended = False
+
+        self._moves = 0
 
         return ts.restart(self._state)
 
@@ -109,29 +115,157 @@ class PyEnv2048(py_environment.PyEnvironment):
 
     def _step(self, action):
 
-        # Reset if game over
+        # Reset if episode over
+        # This code is unreachable, but it is in the tutorial for some reason,
+        # so I put it here as well, maybe it has some use I don't know of?
         if self._episode_ended:
             return self.reset()
 
-        #TODO
+        self._moves += 1
+        if self._moves >= self.MAX_MOVES:
+            self._episode_ended = True
 
-        #DEFINE "reward" here:
+        #??? Somehow refactor to remove a lot of dublicate code?
+
+        # list for tiles already merged this move
+        merged = []
+        reward = 0
+
+        moved = False
+
+        # move up
         if action == 0:
-            # move up
-            pass
+
+            # Starts at the top
+            for y in range(4):
+                for x in range(4):
+
+                    # only moves non-zero tiles
+                    if (tile_value := self._state[y][x]) != 0:
+
+                        new_y = y
+
+                        # Moves the tile up as far as it can go
+                        while new_y > 0 and self._state[new_y-1][x] == 0:
+                            new_y -= 1
+
+                        # Checks if the tile can be merged, and merges
+                        if new_y > 0 \
+                            and tile_value == self._state[new_y-1][x] \
+                            and (new_y - 1, x) not in merged:
+
+                                # Sets the old location to 0
+                                self._state[y][x] = 0
+                                # Doubles the new location (merge)
+                                self._state[new_y-1][x] *= 2
+                                # Appends tile to merged list
+                                merged.append((new_y-1, x))
+                                # Adds reward
+                                reward += tile_value * 2
+                                # Sets moved to True
+                                moved = True
+
+                        # If it can not be merged, just moves it
+                        elif new_y != y:
+                            self._state[y][x] = 0
+                            self._state[new_y][x] = tile_value
+                            moved = True
+
+
+        # move right
         elif action == 1:
-            # move right
-            pass
+
+            for y in range(4):
+                # Start at the far right
+                for x in range(3, -1, -1):
+
+                    if (tile_value := self._state[y][x]) != 0:
+                        new_x = x
+
+                        while new_x < 3 and self._state[y][new_x+1] == 0:
+                            new_x += 1
+
+                        if new_x < 3 \
+                            and tile_value == self._state[y][new_x + 1] \
+                            and (y, new_x + 1) not in merged:
+
+                                self._state[y][x] = 0
+                                self._state[y][new_x+1] *= 2
+                                merged.append((y, new_x+1))
+                                reward += tile_value * 2
+                                moved = True
+
+                        elif new_x != x:
+                            self._state[y][x] = 0
+                            self._state[y][new_x] = tile_value
+                            moved = True
+
+        # move down
         elif action == 2:
-            # move down
-            pass
+
+            # start at the bottom
+            for y in range(3, -1, -1):
+                for x in range(4):
+                    if (tile_value := self._state[y][x]) != 0:
+                        new_y = y
+
+                        while new_y < 3 and self._state[new_y+1][x] == 0:
+                            new_y += 1
+
+                        if new_y < 3 \
+                            and tile_value == self._state[new_y+1][x] \
+                            and (new_y + 1, x) not in merged:
+
+                                self._state[y][x] = 0
+                                self._state[new_y+1][x] *= 2
+                                merged.append((new_y+1, x))
+                                reward += tile_value * 2
+                                moved = True
+
+                        elif new_y != y:
+                            self._state[y][x] = 0
+                            self._state[new_y][x] = tile_value
+                            moved = True
+
+        # move left
         elif action == 3:
-            # move left
-            pass
+
+            for y in range(4):
+                # start at the far left
+                for x in range(4):
+                    if (tile_value := self._state[y][x]) != 0:
+                        new_x = x
+
+                        while new_x > 0 and self._state[y][new_x-1] == 0:
+                            new_x -= 1
+
+                        if new_x > 0 \
+                            and tile_value == self._state[y][new_x-1] \
+                            and (y, new_x - 1) not in merged:
+
+                                self._state[y][x] = 0
+                                self._state[y][new_x - 1] *= 2
+                                merged.append((y, new_x - 1))
+                                reward += tile_value * 2
+                                moved = True
+
+                        elif new_x != x:
+                            self._state[y][x] = 0
+                            self._state[y][new_x] = tile_value
+                            moved = True
+
+        # If moved, add new tile
+        if moved:
+            self.__new_tile()
+
+        discount = 1
+        #!!! Possible to add discount as to not discourage moves that do not
+        # give any points.
+        # if reward == 0 and moved:
+        #     discount = 1.1
 
         # Check whether game has ended, or if a set number of moves was made
         if self._episode_ended or self.__gameover():
             return ts.termination(self._state, reward)
         else:
-            return ts.transition(self._state, reward)
-
+            return ts.transition(self._state, reward, discount=discount)
